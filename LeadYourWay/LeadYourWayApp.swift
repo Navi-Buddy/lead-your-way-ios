cc2521c7978c3f0e1f7b504d3221352b05f9b981
Navarro <u201713141@upc.edu.pe>
2023-09-18 02:01:46 -0500

    Initial Commit

---
+++ LeadYourWay/LeadYourWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeadYourWayApp.swift
+//  LeadYourWay
+//
+//  Created by user245074 on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LeadYourWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
